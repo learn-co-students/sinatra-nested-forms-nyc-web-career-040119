ca61091e5cd5355d534ac6838c9e856da64d1758
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-23 16:40:45 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  @@all = []
+
+  attr_reader :name, :weight, :height
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
